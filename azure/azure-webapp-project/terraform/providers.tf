a3a8dcc8a07461b52c88b8aebf7ae151534a5383
Emre Baykal <emrebaykal@gmail.com>
2025-01-22 18:58:18 +0300

    providers

--- azure/azure-webapp-project/terraform/providers.tf
+++ azure/azure-webapp-project/terraform/providers.tf
@@ -1,14 +1,13 @@
+# Configure the Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0.2"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
+
+  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
