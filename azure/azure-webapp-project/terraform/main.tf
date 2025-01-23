c6e86d62afbf21afe0c106be4b222dac605ba750
Emre Baykal <emrebaykal@gmail.com>
2025-01-23 14:16:19 +0300

    node.js

--- azure/azure-webapp-project/terraform/main.tf
+++ azure/azure-webapp-project/terraform/main.tf
@@ -1,57 +1,54 @@
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
+# Generate a random integer to create a globally unique name
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
 }
 
+# Create the resource group
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.project_name}-${var.environment}-rg"
+  name     = "${var.project_name}-${var.environment}-${random_integer.ri.result}"
   location = var.location
 }
 
-resource "azurerm_service_plan" "plan" {
-  name                = "${var.project_name}-${var.environment}-plan"
+# Create the Linux App Service Plan
+resource "azurerm_service_plan" "appserviceplan" {
+  name                = "${var.project_name}-${var.environment}-${random_integer.ri.result}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "${var.project_name}${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "container" {
-  name                  = "webcontent"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "blob" {
-  name                   = "site.zip"
-  storage_account_name   = azurerm_storage_account.storage.name
-  storage_container_name = azurerm_storage_container.container.name
-  type                   = "Block"
-  source                 = "${path.module}/../src/website/site.zip"
-}
-
+# Create the web app, pass in the App Service Plan ID
 resource "azurerm_linux_web_app" "webapp" {
-  name                = "${var.project_name}-${var.environment}-${random_string.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
+  name                  = "webapp-${random_integer.ri.result}"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  service_plan_id       = azurerm_service_plan.appserviceplan.id
+  depends_on            = [azurerm_service_plan.appserviceplan]
+  https_only            = true
 
   site_config {
     application_stack {
       node_version = "16-lts"
     }
+    minimum_tls_version = "1.2"
   }
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net/${azurerm_storage_container.container.name}/${azurerm_storage_blob.blob.name}?${azurerm_storage_account.storage.primary_access_key}"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
+}
+
+# Deploy code from a public GitHub repo
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                 = azurerm_linux_web_app.webapp.id
+  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  branch                 = "main"
+  use_manual_integration = true
+  use_mercurial          = false
+}
+
+output "webapp_url" {
+  value = "https://${azurerm_linux_web_app.webapp.default_hostname}"
 }
