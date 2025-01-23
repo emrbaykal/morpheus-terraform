c47e93ca423bbc47a8cc154d6de637a83c055ad0
Emre Baykal <emrebaykal@gmail.com>
2025-01-23 20:14:56 +0300

    new

--- azure/azure-webapp-project/main.tf
+++ azure/azure-webapp-project/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_service_plan" "appserviceplan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "P1v2"
 }
 
 # Create the web app, pass in the App Service Plan ID
@@ -25,29 +25,22 @@ resource "azurerm_linux_web_app" "webapp" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   service_plan_id       = azurerm_service_plan.appserviceplan.id
-  depends_on            = [azurerm_service_plan.appserviceplan]
 
   site_config {
     application_stack {
-      php_version = "7.4"
+      python_version = "3.9"
+
     }
     minimum_tls_version = "1.2"
-  }
-
-  app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = "1"
   }
 }
 
 # Deploy code from a public GitHub repo
 resource "azurerm_app_service_source_control" "sourcecontrol" {
   app_id                 = azurerm_linux_web_app.webapp.id
-  repo_url               = "https://github.com/Azure-Samples/nodejs-docs-hello-world"
+  repo_url               = "https://github.com/Azure-Samples/msdocs-python-flask-webapp-quickstart"
   branch                 = "main"
-  use_manual_integration = true
   use_mercurial          = false
+  use_manual_integration = true
 }
 
-output "webapp_url" {
-  value = "https://${azurerm_linux_web_app.webapp.default_hostname}"
-}
