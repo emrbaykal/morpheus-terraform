6789c5e2aa4a4ba623902ecb266b3bfb0bc1618a
Emre Baykal <emrebaykal@gmail.com>
2025-02-15 19:55:13 +0300

    tst

--- aws/test/main.tf
+++ aws/test/main.tf
@@ -1,5 +1,10 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
+variable "access_key" {
+  type        = string
+}
+
+variable "secret_key" {
+  type        = string
+}
 
 provider "aws" {
   region = "eu-west-1"
